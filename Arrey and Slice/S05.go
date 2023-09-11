1e4c6bd76f9562cd74dba67bca10602eab0d3d46
Marcos Vinícius <98711516+vrsmarcos26@users.noreply.github.com>
2023-09-11 08:36:03 -0300

    Update S05.go

--- Arrey and Slice/S05.go
+++ Arrey and Slice/S05.go
@@ -3,23 +3,21 @@
 // Em seguida, imprima a matriz resultante.
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var matriz [3][2]int
-
-	for i := 0; i < 3; i++ {
-		for n := 0; n < 2; n++ {
-			fmt.Printf("Informe o valor da matriz[%d][%d]: ", i, n)
-			fmt.Scan(&matriz[i][n])
-		}
-	}
-	fmt.Println("Matriz resultante:")
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 2; j++ {
-			fmt.Printf("%d ", matriz[i][j])
+			fmt.Printf("Informe o valor da da linha[%d] coluna [%d]: ", i, j)
+			fmt.Scan(&matriz[i][j])
 		}
-		fmt.Println()
+	}
+
+	for _, ints := range matriz {
+		fmt.Print(ints)
 	}
 
 }
